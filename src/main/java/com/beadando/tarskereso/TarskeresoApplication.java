690502418f10ebabf6f9124a463a2c74000553f9
TolliT <tollitician@gmail.com>
2023-01-02 18:22:38 +0100

    Initial commit

---
+++ src/main/java/com/beadando/tarskereso/TarskeresoApplication.java
@@ -0,0 +1,13 @@
+package com.beadando.tarskereso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarskeresoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarskeresoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beadando/tarskereso/TarskeresoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beadando.tarskereso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarskeresoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
