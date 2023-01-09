17904d29bb8b0b210e3f1f280027cd3de795e46a
TolliT <tollitician@gmail.com>
2023-01-09 22:41:50 +0100

    Test function for receiving preferences from frontend

--- server/src/main/java/com/beadando/tarskereso/Controller/UserServiceController.java
+++ server/src/main/java/com/beadando/tarskereso/Controller/UserServiceController.java
@@ -5,13 +5,11 @@ import com.beadando.tarskereso.model.Nem_enum;
 import com.beadando.tarskereso.model.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 
 @RestController
@@ -27,7 +25,17 @@ public class UserServiceController {
     }
 
 
+    @CrossOrigin
+    @PostMapping("/prefs")
+    public ResponseEntity<String> submit(@RequestBody Map<String, Object> payload) {
+        for (Map.Entry<String, Object> entry : payload.entrySet()) {
+            System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+        }
+        return ResponseEntity.ok("success");
+    }
 }
+
+
 
 
 
