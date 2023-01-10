4f0beda44b41d5471d534c7d70863729069f908c
TolliT <tollitician@gmail.com>
2023-01-10 12:26:29 +0100

    Adding constraints to preferences

--- client/swipeHandler.js
+++ client/swipeHandler.js
@@ -1,7 +1,11 @@
 const GETUrl = "http://localhost:8080/getuser";
 const POSTUrl = "http://localhost:8080/prefs";
 
-const cards = document.querySelectorAll('.card');
+const male_ph = "placeholder.png";
+const female_ph = "placeholder_female.png";
+
+const cards = document.querySelector('.card');
+const image = cards.querySelector("img");
 const likeButton = document.querySelector('.like');
 const dislikeButton = document.querySelector('.dislike');
 
@@ -15,7 +19,35 @@ const form = document.querySelector('form');
 const infoLine = document.querySelector('.infoLine');
 const bioLine = document.querySelector('.bioLine');
 
+
+window.onload = function() {
+  init();
+};
+
+function init() {
+  newCard();
+}
+
+
+
 submitButton.addEventListener('click', () => {
+    if(ageMin.value < 18 || ageMin.value > 99){
+      console.log("Minimum age value is incorrect!")
+      ageMin.value = 18;
+      return;
+    }
+
+    if(ageMax.value < 18 || ageMax.value > 99){
+      console.log("Maximum age value is incorrect!")
+      ageMax.value = 99;
+      return;
+    }
+
+    if(ageMax.value < ageMin.value){
+      console.log("Minimum age must be smaller than maximum age!")
+      ageMax.value = parseInt(ageMin.value) + 1;
+      return;
+    }
 
     const POSTData = {
       genderPref: form.elements.gender.value,
@@ -36,18 +68,22 @@ dislikeButton.addEventListener('click', () => {
   newCard();
 });
 
+
+
 function newCard() {
   getData(GETUrl)
   .then(response => {
     infoLine.textContent = response.nev + " (" + response.kor + ")";
     bioLine.textContent = response.leiras;
-    // response.name
+    if(response.nem == "ferfi"){
+      image.src = male_ph
+    }
+    else{
+      image.src = female_ph
+    }
   })
   .catch(error => {
     console.log("ERROR: API call failed!");
-    //REMOVE
-    infoLine.textContent = "ERROR (ERROR)";
-    bioLine.textContent = "Failed to get data.";
   });
 }
 
