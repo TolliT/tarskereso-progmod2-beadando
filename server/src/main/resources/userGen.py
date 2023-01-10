99b90add243c81a944499698d247e94472cc085f
TolliT <tollitician@gmail.com>
2023-01-10 11:59:24 +0100

    Added bio generation to python script, minor gender select fix

--- server/src/main/resources/userGen.py
+++ server/src/main/resources/userGen.py
@@ -6,6 +6,10 @@ num = 0
 data = []
 database = "Tarskereso_db.csv"
 
+verbs = ["hate", "love", "can't stand", "adore", "despise", "admire", "prefer"]
+nouns = ["cats", "dogs", "traveling", "drinking", "hiking", "playing board games", "commiting war crimes"]
+endings = [".", "...", "!", "!!!"]
+
 while( num < 1 or num > 1000):
     try:
         num = int(input("Number of users to generate: "))
@@ -16,6 +20,12 @@ for i in range(num):
     g = "male"
     g_hu = "ferfi"
     like = False
+    
+    v_rand = verbs[random.randrange(0, len(verbs))]
+    n_rand = nouns[random.randrange(0, len(nouns))]
+    e_rand = endings[random.randrange(0, len(endings))]
+    
+    bio = "I " + v_rand + " " + n_rand + e_rand
     
     like_rand = random.randrange(0,4)
     g_rand = random.randrange(0,2)
@@ -29,7 +39,7 @@ for i in range(num):
     
     name = names.get_full_name(gender=g)
     age = random.randrange(18,100)
-    a_data = [i, g_hu, name, age, "bio placeholder", like]
+    a_data = [i, g_hu, name, age, bio, like]
     data.append(a_data)
 
 with open(database, 'w', newline='') as csvfile:
